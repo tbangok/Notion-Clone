f6999a0a3a828bbf2ed77cfdfdf5dc7b97543577
tbangok <tabaoanhngoc10@gmail.com>
2024-04-22 10:04:08 +0700

    fix: :lipstick: Update image

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -4,7 +4,7 @@ export const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl ">
       <div className="flex items-center">
-        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[300px] md:h-[300px]">
             <Image
             src='/document.png'
             fill
@@ -19,7 +19,7 @@ export const Heroes = () => {
             />
         </div>
 
-        <div className="relative w-[400px] h-[400px] hidden md:block" >
+        <div className="relative w-[300px] h-[300px] hidden md:block" >
             <Image 
               src='/reading.png'
               fill
